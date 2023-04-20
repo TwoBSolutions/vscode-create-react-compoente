8c76d1f1f0c8db06de32e348ab7300766e845f37
Jeandro Couto <jeandro.couto@midway.com.br>
2023-04-19 23:02:45 -0300

    adicionando opcao por pasta

--- out/extension.js
+++ out/extension.js
@@ -5,57 +5,54 @@ const vscode = require("vscode");
 const fs = require("fs");
 const path = require("path");
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('createreactcomponent', async () => {
-        const componentName = await vscode.window.showInputBox({ prompt: 'Nome do componente' });
+    let disposable = vscode.commands.registerCommand("createreactcomponent", async (uri) => {
+        const componentName = await vscode.window.showInputBox({
+            prompt: "Nome do componente",
+        });
         if (!componentName) {
             return;
         }
-        const componentType = await vscode.window.showQuickPick(['React', 'React Native'], { placeHolder: 'Selecione o tipo de componente' });
+        const componentType = await vscode.window.showQuickPick(["React", "React Native"], { placeHolder: "Selecione o tipo de componente" });
         if (!componentType) {
             return;
         }
-        const fileType = await vscode.window.showQuickPick(['tsx', 'js', 'jsx'], { placeHolder: 'Selecione o tipo de arquivo' });
+        const fileType = await vscode.window.showQuickPick(["tsx", "js", "jsx"], {
+            placeHolder: "Selecione o tipo de arquivo",
+        });
         if (!fileType) {
             return;
         }
-        const folderUri = await vscode.window.showOpenDialog({
-            canSelectFolders: true,
-            canSelectFiles: false,
-            canSelectMany: false,
-            openLabel: 'Selecione a pasta do componente'
-        });
-        if (!folderUri || folderUri.length === 0) {
-            return;
-        }
-        const currentDir = folderUri[0].fsPath;
+        const currentDir = uri.fsPath;
         if (currentDir) {
-            console.log("🚀 ~ file: extension.ts:25 ~ disposable ~ currentDir:", currentDir);
             const componentDir = path.join(currentDir, componentName);
-            console.log("🚀 ~ file: extension.ts:26 ~ disposable ~ componentDir:", componentDir);
             if (!fs.existsSync(componentDir)) {
                 fs.mkdirSync(componentDir);
             }
             // Criação do arquivo Component.extensao
-            const componentTemplate = generateComponentTemplate(componentName, componentType === 'React Native');
+            const componentTemplate = generateComponentTemplate(componentName, componentType === "React Native");
             const componentPath = path.join(componentDir, `${componentName}.${fileType}`);
             fs.writeFileSync(componentPath, componentTemplate);
             // Criação do arquivo component.styled.extensao
             const styledTemplate = generateStyledTemplate();
-            const styledPath = path.join(componentDir, `${componentName.toLowerCase()}.styled.${fileType === 'tsx' ? 'ts' : fileType}`);
+            const styledPath = path.join(componentDir, `${componentName.toLowerCase()}.styled.${fileType === "tsx" ? "ts" : fileType}`);
             fs.writeFileSync(styledPath, styledTemplate);
             // Criação do arquivo index.extensao
-            const indexPath = path.join(componentDir, `index.${fileType === 'tsx' ? 'ts' : fileType}`);
+            const indexPath = path.join(componentDir, `index.${fileType === "tsx" ? "ts" : fileType}`);
             const indexTemplate = `export { default } from './${componentName}';\n`;
             fs.writeFileSync(indexPath, indexTemplate);
             const openPath = vscode.Uri.file(componentPath);
-            vscode.workspace.openTextDocument(openPath).then((doc) => vscode.window.showTextDocument(doc));
+            vscode.workspace
+                .openTextDocument(openPath)
+                .then((doc) => vscode.window.showTextDocument(doc));
         }
     });
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 function generateComponentTemplate(componentName, isReactNative) {
-    const importStatement = isReactNative ? "import { View, Text } from 'react-native';" : "import React from 'react';";
+    const importStatement = isReactNative
+        ? "import { View, Text } from 'react-native';"
+        : "import React from 'react';";
     const componentTemplate = `
 ${importStatement}
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,69 +1,88 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('createreactcomponent', async () => {
-    const componentName = await vscode.window.showInputBox({ prompt: 'Nome do componente' });
-    if (!componentName) {
-      return;
-    }
-
-    const componentType = await vscode.window.showQuickPick(['React', 'React Native'], { placeHolder: 'Selecione o tipo de componente' });
-    if (!componentType) {
-      return;
-    }
-
-    const fileType = await vscode.window.showQuickPick(['tsx', 'js', 'jsx'], { placeHolder: 'Selecione o tipo de arquivo' });
-    if (!fileType) {
-      return;
-    }
-
-    const folderUri = await vscode.window.showOpenDialog({
-      canSelectFolders: true,
-      canSelectFiles: false,
-      canSelectMany: false,
-      openLabel: 'Selecione a pasta do componente'
-    });
-    
-    if (!folderUri || folderUri.length === 0) {
-      return;
-    }
-    
-    const currentDir = folderUri[0].fsPath;
-    if (currentDir) {
-      console.log("🚀 ~ file: extension.ts:25 ~ disposable ~ currentDir:", currentDir)
-      const componentDir = path.join(currentDir, componentName);
-      console.log("🚀 ~ file: extension.ts:26 ~ disposable ~ componentDir:", componentDir)
-      if (!fs.existsSync(componentDir)) {
-        fs.mkdirSync(componentDir);
+  let disposable = vscode.commands.registerCommand(
+    "createreactcomponent",
+    async (uri: vscode.Uri) => {
+      const componentName = await vscode.window.showInputBox({
+        prompt: "Nome do componente",
+      });
+      if (!componentName) {
+        return;
       }
 
-      // Criação do arquivo Component.extensao
-      const componentTemplate = generateComponentTemplate(componentName, componentType === 'React Native');
-      const componentPath = path.join(componentDir, `${componentName}.${fileType}`);
-      fs.writeFileSync(componentPath, componentTemplate);
+      const componentType = await vscode.window.showQuickPick(
+        ["React", "React Native"],
+        { placeHolder: "Selecione o tipo de componente" }
+      );
+      if (!componentType) {
+        return;
+      }
 
-      // Criação do arquivo component.styled.extensao
-      const styledTemplate = generateStyledTemplate();
-      const styledPath = path.join(componentDir, `${componentName.toLowerCase()}.styled.${fileType === 'tsx' ? 'ts' : fileType}`);
-      fs.writeFileSync(styledPath, styledTemplate);
+      const fileType = await vscode.window.showQuickPick(["tsx", "js", "jsx"], {
+        placeHolder: "Selecione o tipo de arquivo",
+      });
+      if (!fileType) {
+        return;
+      }
 
-      // Criação do arquivo index.extensao
-      const indexPath = path.join(componentDir, `index.${fileType === 'tsx' ? 'ts' : fileType}`);
-      const indexTemplate = `export { default } from './${componentName}';\n`;
-      fs.writeFileSync(indexPath, indexTemplate);
+      const currentDir = uri.fsPath;
+      if (currentDir) {
+        const componentDir = path.join(currentDir, componentName);
 
-      const openPath = vscode.Uri.file(componentPath);
-      vscode.workspace.openTextDocument(openPath).then((doc) => vscode.window.showTextDocument(doc));
+        if (!fs.existsSync(componentDir)) {
+          fs.mkdirSync(componentDir);
+        }
+
+        // Criação do arquivo Component.extensao
+        const componentTemplate = generateComponentTemplate(
+          componentName,
+          componentType === "React Native"
+        );
+        const componentPath = path.join(
+          componentDir,
+          `${componentName}.${fileType}`
+        );
+        fs.writeFileSync(componentPath, componentTemplate);
+
+        // Criação do arquivo component.styled.extensao
+        const styledTemplate = generateStyledTemplate();
+        const styledPath = path.join(
+          componentDir,
+          `${componentName.toLowerCase()}.styled.${
+            fileType === "tsx" ? "ts" : fileType
+          }`
+        );
+        fs.writeFileSync(styledPath, styledTemplate);
+
+        // Criação do arquivo index.extensao
+        const indexPath = path.join(
+          componentDir,
+          `index.${fileType === "tsx" ? "ts" : fileType}`
+        );
+        const indexTemplate = `export { default } from './${componentName}';\n`;
+        fs.writeFileSync(indexPath, indexTemplate);
+
+        const openPath = vscode.Uri.file(componentPath);
+        vscode.workspace
+          .openTextDocument(openPath)
+          .then((doc) => vscode.window.showTextDocument(doc));
+      }
     }
-  });
+  );
 
   context.subscriptions.push(disposable);
 }
 
-function generateComponentTemplate(componentName: string, isReactNative: boolean): string {
-  const importStatement = isReactNative ? "import { View, Text } from 'react-native';" : "import React from 'react';";
+function generateComponentTemplate(
+  componentName: string,
+  isReactNative: boolean
+): string {
+  const importStatement = isReactNative
+    ? "import { View, Text } from 'react-native';"
+    : "import React from 'react';";
   const componentTemplate = `
 ${importStatement}
 
